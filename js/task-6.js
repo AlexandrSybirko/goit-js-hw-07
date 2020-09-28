a96a2bc1ba731b9a7a1b6b9a6a4058f26ad772ed
AlexandrSybirko <63520677+AlexandrSybirko@users.noreply.github.com>
2020-09-28 18:12:15 +0300

    Исправляет 6 задание

--- js/task-6.js
+++ js/task-6.js
@@ -7,7 +7,12 @@ const checkLength = Number(inputRef.dataset.length);
 function check() {
   if (inputRef.value.length === checkLength) {
     inputRef.classList.add('valid');
+    inputRef.classList.remove('invalid');
+  } else if (inputRef.value.length === 0) {
+    inputRef.classList.remove('invalid');
+    inputRef.classList.remove('valid');
   } else {
     inputRef.classList.add('invalid');
+    inputRef.classList.remove('valid');
   }
 }
